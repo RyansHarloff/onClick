110d744e1c89435588826d14e14736fea4b22cce
Ryan Harloff <ryans.harloff@gmail.com>
2021-04-26 13:40:08 -0500

    onClick

---
+++ script.js
@@ -0,0 +1,11 @@
+function displayName() {
+    console.log('The element is Hydrogen');
+}
+
+function displayName2(){
+    console.log('The element is Helium');
+}
+
+function displayName3(){
+    console.log('The element is Lithium');
+}
